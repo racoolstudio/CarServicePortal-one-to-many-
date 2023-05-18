e2863e8dea540ebcb5be6b98fb5aaddfcf3c0d46
racoolstudio <75172422+racoolstudio@users.noreply.github.com>
2023-05-18 13:54:16 -0230

    #database

---
+++ database/migrations/2023_05_15_032555_create_cars_table.php
@@ -0,0 +1,33 @@
+
+<?php
+
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('car_name');
+            $table->string('car_model');
+            $table->unsignedBigInteger('owner_id');
+            $table->foreign('owner_id')->references('id')->on('owners');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
