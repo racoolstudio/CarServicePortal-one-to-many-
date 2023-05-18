8445cfcff77a8746c3feb73e3fa661fe76aa0c36
racoolstudio <75172422+racoolstudio@users.noreply.github.com>
2023-05-18 13:52:26 -0230

    @database

---
+++ database/factories/OwnerFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Owner>
+ */
+
+ use Faker\Factory as FakerFactory;
+
+class OwnerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $faker = FakerFactory::create();
+
+        return [
+            
+            'first_name' => $faker->firstName(),
+            'last_name' => $faker->lastName(),
+            ];
+    }
+}
